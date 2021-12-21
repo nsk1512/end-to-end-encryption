a4386304b677043177846dee839244432bcb61f9
Nandita Kadam <65679261+nsk1512@users.noreply.github.com>
2021-12-22 02:20:16 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,131 @@
+import socket
+import select
+import os
+import configparser
+from time import gmtime, strftime
+import sys
+import rsa
+def get_config(config_file, category, value):
+# SETUP CONFIGPARSER
+DIR = os.path.dirname(os.path.realpath(__file__))
+conf_file = os.path.join(DIR, config_file)
+config = configparser.ConfigParser()
+config.read(conf_file)
+return config.get(category, value)
+def init_msg(ip, port):
+print('Server running')
+print(f'IP: {ip}:{port}\n')
+def log(msg):
+time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+print(f'[{time}]: {msg}')
+def get_header(message):
+return f'{len(message):<{HEADER_LENGTH}}'.encode('utf-8')
+def receive_message(client_socket):
+try:
+# Receive header
+message_header = client_socket.recv(HEADER_LENGTH)
+# If no data was received, client closed the connection
+if not len(message_header):
+return False
+# Get message length from the received header
+message_length = int(message_header.decode('utf-8').strip())
+return {'header': message_header, 'data':
+client_socket.recv(message_length)}
+# Can happen if client breaks their script
+
+except Exception:
+return False
+class Client:
+def __init__(self, uname, pub_key, client_socket, client_address):
+self.socket = client_socket
+self.username_header = uname['header']
+self.raw_username = uname['data']
+self.username = self.raw_username.decode('utf-8')
+if pub_key:
+self.pub_key_header = pub_key['header']
+self.pub_key_pem = pub_key['data']
+self.pub_key = rsa.PublicKey.load_pkcs1(self.pub_key_pem)
+self.ip = client_address[0]
+self.port = client_address[1]
+self.address = f'{self.ip}:{self.port}'
+def process_connection(notified_socket):
+client_socket, client_address = notified_socket.accept()
+uname = receive_message(client_socket)
+pub_key = receive_message(client_socket)
+client = Client(
+uname, pub_key, client_socket, client_address)
+# Handle user disconnect
+if uname is False:
+log(
+f'New connection failed from {client.address}')
+return False
+elif pub_key is False:
+log(
+f'New connection failed from {client.address}, No public key
+provided (username: {client.username})')
+return False
+else:
+# Add user's socket to socket_list
+sockets_list.append(client_socket)
+	
+# Add client's info to clients dict
+clients[client_socket] = client
+log(
+f'Accepted new connection from {client.address}
+username:{client.username}')
+return True
+def process_message(notified_socket):
+def send_to_others(message):
+# Share the message with everyone
+for client_socket in clients:
+# Don't send back to sender
+if client_socket != notified_socket:
+# Send info about sender and the message he sent to other
+clients
+sender_information = client.username_header +
+client.raw_username
+message_to_send = message['header'] + message['data']
+client_socket.send(sender_information + message_to_send)
+message = receive_message(notified_socket)
+signature = receive_message(notified_socket)
+# Connection closed
+if message is False or signature is False:
+client = clients[notified_socket]
+log(f'Closed connection from {client.username} ({client.address})')
+# Remove disconnected socket from sockets_list
+sockets_list.remove(notified_socket)
+# Remove disconnected socket from clients (dict)
+del clients[notified_socket]
+return False
+client = clients[notified_socket]
+# GET BASIC PARAMS FROM CONFIG
+config_file = 'config.conf'
+	
+HEADER_LENGTH = int(get_config(config_file, 'Main', 'HEADER_LENGTH'))
+IP = get_config(config_file, 'Main', 'IP')
+PORT = int(get_config(config_file, 'Main', 'PORT'))
+# Show starting message
+init_msg(IP, PORT)
+# SETUP SERVER_SOCKET
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Allow to reconnect (fix Address in use after restart)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# Set IP (and port)
+server_socket.bind((IP, PORT))
+server_socket.listen()
+sockets_list = [server_socket] # List of known sockets
+clients = {} # Contains Client class with client's information
+while True:
+try:
+read_sockets, write_sockets, exception_sockets = select.select(
+sockets_list, [], sockets_list)
+except KeyboardInterrupt:
+print('\n\nServer stopped.. (KeyboardInterrupt)')
+sys.exit()
+for notified_socket in read_sockets:
+# Someone just connected
+if notified_socket == server_socket:
+process_connection(notified_socket)
+# Someone sent a message/left
+else:
+process_message(notified_socket)
